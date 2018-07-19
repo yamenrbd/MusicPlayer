be14518c463127e154874fd8a55b0660b2f07baf
yamen_rbd <yamenrbd@gmail.com>
2018-07-19 17:29:09 +0300

    19/7/2018

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.android.musicplayer;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer = MediaPlayer.create(this,R.raw.adele);
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.color_black);
+
+
         mPlay = (Button) findViewById(R.id.play);
         mPause = (Button) findViewById(R.id.pause);
 
@@ -35,7 +42,35 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                releaseMediaPlayer();
+
+            }
+        });
     }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+            Toast.makeText(MainActivity.this, "the resource is clear", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
 
 }
